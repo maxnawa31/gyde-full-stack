85b6fcce3d404d4fc6e773d23cf4b6271ed49cff
maxnawa31 <mnawa31@gmail.com>
2018-06-21 20:31:37 -0700

    homepage finished

--- gyde-client/src/components/Homepage.js
+++ gyde-client/src/components/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => (
       <div className ='left-half'>
       <p className='heading'>We are here to<br/> change Ed-Tech<br/> for the better</p>
 
-      <p className='sub-heading'>Keeping your entire team on the same page is a huge undertaking - but it’s the vital component to getting work done efficiently and collaboratively. Loom enables every team to work together, all in the same space.</p>
+      <p className='sub-heading'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
         <div className='button-container'>
       <button className='signup-button'><Link to = '/signup' >Sign Up Here </Link></button>
@@ -20,18 +20,30 @@ const Homepage = () => (
           <div className='circle-green'/>
           <h3>Connect with professors</h3>
         </div>
+        <div className='text-column'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>
         <div className='bullet'>
           <div className='circle-blue'/>
           <h3>Share your research to the world</h3>
+        </div>
+        <div className='text-column'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
         </div>
       <div className='bullet'>
         <div className='circle-purple'></div>
         <h3>Expand your knowledge</h3>
       </div>
+      <div className='text-column'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>
       <div className='bullet'>
         <div className='circle-red'></div>
         <h3>Contribute to a community of learners</h3>
       </div>
+      <div className='text-column'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>
     </div>
     </div>
 
